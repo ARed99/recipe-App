0991496a969e66b01d648418f319528d2d45b0bc
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-01 19:42:51 +0530

    added navbar to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
+    import Navbar from '../componets/Navbar,.svelte';
+import type { LayoutData } from './$types';
     
     export let data: LayoutData;
 </script>
-
+<Navbar/>
 <slot>
 
 </slot>
