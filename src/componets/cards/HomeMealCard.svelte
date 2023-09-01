5b55a209e30e7f9be1a6b4c9c93841b867c2f0aa
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-01 19:54:09 +0530

    added RecipeViewBtn componet

---
+++ src/componets/cards/HomeMealCard.svelte
@@ -0,0 +1,12 @@
+<script>
+	import RecipeViewBtn from "../Buttons/RecipeViewBtn.svelte";
+
+</script>
+<div class="home-card">
+    <div class="thumb">
+        <img src="https://i.pinimg.com/736x/77/a2/0e/77a20e280fc4564d7059adef760cf581.jpg" alt="" width="300px" height="400px">
+    </div>
+    <div class="recipe-link">
+        <RecipeViewBtn recipeUrl={"dasdad"}/>
+    </div>
+</div>
