18c5b6773f35270c46fcd4d08a98d2aea34c47f3
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-01 19:41:47 +0530

    nav bar

---
+++ src/componets/Navbar,.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Search from "./Search.svelte";
+
+</script>
+<div class="nav-bar">
+    <div class="title">
+        <h1>
+            Recipe App
+        </h1>
+    </div>
+   
+</div>
