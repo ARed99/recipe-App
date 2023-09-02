8610068e8ce7a702a0797013d4ce81d136da3d44
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-02 15:48:22 +0530

    RecipeViiewCard stype completed

--- src/componets/Buttons/RecipeViewBtn.svelte
+++ src/componets/Buttons/RecipeViewBtn.svelte
@@ -5,3 +5,23 @@
 <div class="recipe-btn">
     <a href={recipeUrl}>View Recipe ↗</a>
 </div>
+
+<style>
+    .recipe-btn{
+        
+        width: 100%;
+        height: 100px;
+ 
+      display: grid;
+      place-items: end;
+      justify-content: center;
+      
+    }
+    .recipe-btn a{
+        font-size: 30px;
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+        text-decoration: none;
+        color:   #347E68;
+        
+    }
+</style>
