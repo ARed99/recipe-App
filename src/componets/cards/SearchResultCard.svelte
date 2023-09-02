37604e10c53ff805ee5f46537711dff00823f009
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-02 20:22:19 +0530

    completed style of SearchResultCard

--- src/componets/cards/SearchResultCard.svelte
+++ src/componets/cards/SearchResultCard.svelte
@@ -4,7 +4,7 @@
 </script>
 <div class="search-result-card">
     <div class="thumb">
-        <img src="" alt="">
+        <img src="https://www.themealdb.com/images/media/meals/vpxyqt1511464175.jpg" alt="">
     </div>
     <div class="details">
         <h1>
@@ -16,8 +16,49 @@
         <h3>
            Category:
         </h3>
-        <span>
+        <div class="btn">
             <RecipeViewBtn url={"fasdff"}/>
-        </span>
+        </div>
     </div>
 </div>
+
+<style>
+    .search-result-card{
+        width: 90%;
+        height: 300px;
+     
+        display: flex;
+        justify-content: start;
+        -webkit-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+    }
+    .thumb{
+        width: 400px;
+        height: 300px;
+    }
+    .thumb img{
+        width: 400px;
+        height: 300px;
+        object-fit: cover;
+    }
+    .details{
+        padding: 20px;
+    }
+    h1{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        text-transform: uppercase;
+        font-size: 50px;
+        width: 100%;
+        word-break: break-all;
+    }
+    h3{
+        font-family: monospace;
+        font-size: 40px;
+    }
+    .btn{
+        
+        width: 200px;
+        
+    }
+</style>
