630b78a1660747a28e2c5e2d816c9fad3b38bea2
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-02 20:21:52 +0530

    struture SearchResultCard

---
+++ src/componets/cards/SearchResultCard.svelte
@@ -0,0 +1,23 @@
+<script>
+	import RecipeViewBtn from "../Buttons/RecipeViewBtn.svelte";
+
+</script>
+<div class="search-result-card">
+    <div class="thumb">
+        <img src="" alt="">
+    </div>
+    <div class="details">
+        <h1>
+            meal name here
+        </h1>
+        <h3>
+            Area:
+        </h3>
+        <h3>
+           Category:
+        </h3>
+        <span>
+            <RecipeViewBtn url={"fasdff"}/>
+        </span>
+    </div>
+</div>
