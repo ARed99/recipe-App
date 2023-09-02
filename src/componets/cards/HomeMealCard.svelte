be0921d0f579e6291f8f1973ac6f3a79bed320f7
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-02 15:48:51 +0530

    chnaged test img url

--- src/componets/cards/HomeMealCard.svelte
+++ src/componets/cards/HomeMealCard.svelte
@@ -4,9 +4,34 @@
 </script>
 <div class="home-card">
     <div class="thumb">
-        <img src="https://i.pinimg.com/736x/77/a2/0e/77a20e280fc4564d7059adef760cf581.jpg" alt="" width="300px" height="400px">
+        <img src="https://www.themealdb.com/images/media/meals/vpxyqt1511464175.jpg" alt="" width="300px" height="400px">
     </div>
     <div class="recipe-link">
         <RecipeViewBtn recipeUrl={"dasdad"}/>
     </div>
 </div>
+
+<style>
+    .home-card{
+        width: 300px;
+        height: 300px;
+        border-radius: 20px;
+    
+        -webkit-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+
+    }
+    .thumb{
+        width: 100%;
+        height: 200px;
+    }
+    .thumb img{
+        height: 250px;
+        object-fit: cover;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        
+    }
+
+</style>
