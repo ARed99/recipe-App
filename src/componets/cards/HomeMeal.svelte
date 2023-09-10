fce8d3e9c563bcc2289a6e93058884ecaca8563b
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-10 20:03:33 +0530

    formated

--- src/componets/cards/HomeMeal.svelte
+++ src/componets/cards/HomeMeal.svelte
@@ -1,37 +1,35 @@
-<script>
-	import RecipeViewBtn from "../Buttons/RecipeViewBtn.svelte";
-    export let meal;
+<script lang="ts">
+	import RecipeViewBtn from '../Buttons/RecipeViewBtn.svelte';
+	export let meal:object;
 </script>
+
 <div class="home-card">
-    <div class="thumb">
-        <img src={meal.strMealThumb} alt="" width="300px" height="400px">
-    </div>
-    <div class="recipe-link">
-        <RecipeViewBtn url={`/meal/${meal.idMeal}`}/>
-    </div>
+	<div class="thumb">
+		<img src={meal.strMealThumb} alt="" width="300px" height="400px" />
+	</div>
+	<div class="recipe-link">
+		<RecipeViewBtn url={`/meal/${meal.idMeal}`} />
+	</div>
 </div>
 
 <style>
-    .home-card{
-        width: 300px;
-        height: 300px;
-        border-radius: 20px;
-    
-        -webkit-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
--moz-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
-box-shadow: 0px 9px 12px 0px rgba(0,0,0,0.75);
+	.home-card {
+		width: 300px;
+		height: 300px;
+		border-radius: 20px;
 
-    }
-    .thumb{
-        width: 100%;
-        height: 200px;
-    }
-    .thumb img{
-        height: 250px;
-        object-fit: cover;
-        border-top-left-radius: 20px;
-        border-top-right-radius: 20px;
-        
-    }
-
+		-webkit-box-shadow: 0px 9px 12px 0px rgba(0, 0, 0, 0.75);
+		-moz-box-shadow: 0px 9px 12px 0px rgba(0, 0, 0, 0.75);
+		box-shadow: 0px 9px 12px 0px rgba(0, 0, 0, 0.75);
+	}
+	.thumb {
+		width: 100%;
+		height: 200px;
+	}
+	.thumb img {
+		height: 250px;
+		object-fit: cover;
+		border-top-left-radius: 20px;
+		border-top-right-radius: 20px;
+	}
 </style>
