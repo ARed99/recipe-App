daa02381d2cba525cdf1a3d392bbfe2e7e57dcca
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-10 20:47:34 +0530

    view meal

---
+++ src/routes/meal/[id]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import ViewMeal from "../../../componets/cards/ViewMeal.svelte";
+
+	
+export let data;
+
+</script>
+<div class="view">
+    <div class="box">
+        <ViewMeal/>
+
+    </div>
+    
+</div>
+
+<style>
+    .view{
+        width: 100%;
+        
+    }
+    .box{
+        padding: 20px;
+    }
+</style>
