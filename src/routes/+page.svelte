5ef6a8f65b74be4108f07276e1269d9ed30d05bf
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-10 20:03:41 +0530

    formated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,43 @@
 <script>
-	
-	import HomeMeal from "../componets/cards/HomeMeal.svelte";
-	import SearchResult from "../componets/cards/SearchResult.svelte";
+	import { onMount } from 'svelte';
 
-	const MAX_MEAL_NUMBER = 10
-const get = async()=>{
-	let  meals = [];
-	for (let i = 0; i < MAX_MEAL_NUMBER; i++) {
-  const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-  const data = await response.json();
+	import HomeMeal from '../componets/cards/HomeMeal.svelte';
+	onMount(() => {
+		get();
+	});
 
-  meals.push(data.meals[0]); // Add the meal to the array
-}
-return meals
+	const MAX_MEAL_NUMBER = 10;
+	const get = async () => {
+		let meals = [];
+		for (let i = 0; i < MAX_MEAL_NUMBER; i++) {
+			const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+			const data = await response.json();
 
-}
-
-get()
+			meals.push(data.meals[0]); // Add the meal to the array
+		}
+		return meals;
+	};
 </script>
-<div class="box">
-	
-{#await get()}
-	<h1>loading..</h1>
-{:then meals} 
-	{#each meals as meal }
-		<HomeMeal meal={meal}/>
-	{/each}
-{/await}
 
+<div class="box">
+	{#await get()}
+		<h1>loading..</h1>
+	{:then meals}
+		{#each meals as meal}
+			<HomeMeal {meal} />
+		{/each}
+	{/await}
 </div>
 
-
 <style>
-	.box{
+	.box {
 		padding: 20px;
 		display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); /* Adjust column width as needed */
-  gap: 20px; /* Adjust the gap between cards as needed */
-  justify-content: center; 
+		grid-template-columns: repeat(
+			auto-fill,
+			minmax(300px, 1fr)
+		); /* Adjust column width as needed */
+		gap: 20px; /* Adjust the gap between cards as needed */
+		justify-content: center;
 	}
 </style>
