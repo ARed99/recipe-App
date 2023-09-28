ab1086a5cdb83530d6ea0201cfe1d0bc6118e96a
ajinvarghesechandy <chandyajin@gmail.com>
2023-09-28 15:58:43 +0530

    removed useless onMount import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
-	import { onMount } from 'svelte';
-
 	import HomeMeal from '../componets/cards/HomeMeal.svelte';
-	
 
 	const MAX_MEAL_NUMBER = 10;
 	const get = async () => {
+		// This is the main function that fetches the meals and displays them on the page
+		// It uses the HomeMeal component to display each meal
 		let meals = [];
 		for (let i = 0; i < MAX_MEAL_NUMBER; i++) {
 			const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
@@ -15,6 +14,9 @@
 		}
 		return meals;
 	};
+	
+	// The meals are displayed in a grid layout, with a maximum of 10 meals displayed at a time
+	// The grid layout is responsive and will adjust the number of columns based on the screen size
 </script>
 
 <div class="box">
